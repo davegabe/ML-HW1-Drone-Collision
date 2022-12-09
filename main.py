87aea69370fa5f6ba3a320b9fc56e8b37141d59a
Davide <me@davegabe.it>
2022-12-09 01:20:33 +0100

    Main now launch classification and regression tasks

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from matplotlib import pyplot as plt
 import numpy as np
-from classification import main as main_no_angles
+from classification import main as classification
+from regression import main as regression
 from multiprocessing import Pool
 import builtins
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, precision_score, recall_score
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, mean_squared_error, precision_score, r2_score, recall_score
 import seaborn as sns
 
 
@@ -11,14 +12,14 @@ def mute():
     """
     Mute the print function.
     """
-    return
     builtins.print = lambda *args, **kwargs: None
 
 
-def main():
+def main_classification():
     """
     Launch classification model on different seeds and plot the results.
     """
+    print("#"*10 + " Classification " + "#"*10)
     # List of predictions for each seed, each prediction is a dictionary with the name of the classifier as key and (y_test, y_pred) as value
     predicts: list[dict[str, tuple[np.ndarray, np.ndarray]]] = []
 
@@ -27,7 +28,7 @@ def main():
 
     # Launch the classification model on different seeds
     with Pool(n_processes, initializer=mute) as p:
-        predicts = p.starmap(main_no_angles, [(seed, use_angle) for seed in range(n_processes)])
+        predicts = p.starmap(classification, [(seed, use_angle) for seed in range(n_processes)])
 
     # For each classifier, compute the average accuracy, precision, recall and f1-score
     for classifier in predicts[0].keys():
@@ -49,6 +50,7 @@ def main():
         print(f"\tPrecision: {np.mean(precision)}")
         print(f"\tRecall: {np.mean(recall)}")
         print(f"\tF1-score: {np.mean(f1)}")
+        print("")
 
         # Find the best seed
         best_seed = np.argmax(f1)
@@ -74,5 +76,54 @@ def main():
         plt.close()
 
 
+def main_regression():
+    """
+    Launch regression model on different seeds and plot the results.
+    """
+    print("#"*10 + " Regression " + "#"*10)
+    # List of predictions for each seed, each prediction is a dictionary with the name of the regressor as key and (y_test, y_pred) as value
+    predicts: list[dict[str, tuple[np.ndarray, np.ndarray]]] = []
+
+    n_processes = 10
+    use_angle = False
+
+    # Launch the regression model on different seeds
+    with Pool(n_processes, initializer=mute) as p:
+        predicts = p.starmap(regression, [(seed, use_angle) for seed in range(n_processes)])
+
+    # For each regressor, compute the average accuracy, precision, recall and f1-score
+    for regressor in predicts[0].keys():
+        # List of accuracy, precision, recall and f1-score for each seed
+        mse: list[float] = []
+        r2: list[float] = []
+
+        for predict in predicts:
+            y_test, y_pred = predict[regressor]
+            mse.append(mean_squared_error(y_test, y_pred))
+            r2.append(r2_score(y_test, y_pred))
+
+        print(f"{regressor}:")
+        print(f"\tMean squared error: {np.mean(mse)}")
+        print(f"\tR2 score: {np.mean(r2)}")
+        print("")
+
+        # Find the best seed
+        best_seed = np.argmax(r2)
+        y_test, y_pred = predicts[best_seed][regressor]
+        # print(f"\tBest seed: {best_seed}")
+
+        # Scatter plot of the prediction for the best seed
+        plt.title(f'Prediction for {regressor}')
+        plt.xlabel('Sample')
+        plt.ylabel('Class')
+        len_arr = len(y_test)
+        plt.scatter(range(len_arr), y_test, label='Real')
+        plt.scatter(range(len_arr), y_pred, label='Predicted')
+        plt.legend()
+        plt.savefig(f'./results/prediction_{regressor.replace(" ", "_")}.png', dpi=600)
+        plt.close()
+
+
 if __name__ == '__main__':
-    main()
+    main_classification()
+    main_regression()
